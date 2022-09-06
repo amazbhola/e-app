1fedc82df0ec8a20fd52bf23fc710689b2ced51a
amazbhola <amazingbhola@gmail.com>
2022-09-06 22:38:01 +0600

    #5 add spatie/laravel-medialibrary package & Configure media library to use posts, user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Image\Exceptions\InvalidManipulation;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -38,4 +43,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'date_of_birth' => 'date',
     ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('profile_photo');
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        try {
+            $this->addMediaConversion('thumbnail')
+                ->width('100')
+                ->height('100');
+        } catch (InvalidManipulation $e) {
+            Log::error('User addMediaConversion :'.$e->getMessage());
+        }
+    }
 }
