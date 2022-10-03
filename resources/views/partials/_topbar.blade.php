fd2bbf02eea5d85bab8c906a822ceb9de172edbf
amazbhola <amazingbhola@gmail.com>
2022-10-03 19:08:00 +0600

    modify two file

--- resources/views/partials/_topbar.blade.php
+++ resources/views/partials/_topbar.blade.php
@@ -4,8 +4,8 @@
     </div>
     @guest
         <div>
-            <a class="bg-gray-800 text-gray-100 px-3 py-2 hover:bg-gray-900" href="{{ route('auth.index') }}">login</a>
-            <a class="bg-gray-800 text-gray-100 px-3 py-2 hover:bg-gray-900" href="{{ route('auth.create') }}">Sign Up</a>
+            <a class="bg-gray-800 text-gray-100 px-3 py-2 hover:bg-gray-900" href="{{ route('login') }}">login</a>
+            <a class="bg-gray-800 text-gray-100 px-3 py-2 hover:bg-gray-900" href="{{ route('register') }}">Sign Up</a>
 
         </div>
     @endguest
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-route::resource('auth', AuthController::class);
+
 
 Route::resource('category', CategoryController::class);
 Route::resource('post', PostController::class);
